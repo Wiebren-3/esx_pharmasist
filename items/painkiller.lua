b00f3692892617853a2fad8012ef9773e12f67d2
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-08 10:59:59 +0100

    fix (painkiller) fix dat je geheald wordt
    FIxt tevens ook het verwijderen van het item, tegen exploits.

--- items/painkiller.lua
+++ items/painkiller.lua
@@ -1,7 +1,13 @@
-RegisterCommand("painkiller", function(source, args, rawCommand)
+RegisterNetEvent('esx:useItem')
+AddEventHandler('esx:useItem', function(itemName)
+TriggerClientEvent("esx_animations:playEmote", source, "mechanic3")
 	TriggerClientEvent("esx:showNotification", source, "Je nam een pijnstiller, en voelt je langzamerhand steeds beter.")
-    TriggerClientEvent("esx_animations:playEmote", source, "eat")
-	SetTimeout(30000, function()
+    	TriggerClientEvent("esx_animations:playEmote", source, "eat")
+        local xPlayer = ESX.GetPlayerFromId(source)
+       		local playerPed = xPlayer.playerPed
+		xPlayer.removeInventoryItem('tourniquet', 1)	
+SetTimeout(30000, function()
 		TriggerClientEvent("esx:showNotification", source, "Je bent compleet herstelt!")
+		TriggerEvent('esx_status:add', 'health', 100)
 	end)
-end, false)
+end
