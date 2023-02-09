e7d448fd8d1c461b4456a45887c1db6f7a59e82b
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-09 09:16:50 +0100

    fix (tourniquet) fix notification

--- items/tourniquet.lua
+++ items/tourniquet.lua
@@ -6,7 +6,7 @@ TriggerClientEvent("esx_animations:playEmote", source, "mechanic3")
         local xPlayer = ESX.GetPlayerFromId(source)
         local playerPed = xPlayer.playerPed
         TriggerEvent('esx_status:add', 'health', 100)
-        TriggerEvent('mythic_notify:client:SendAlert', { type = 'success', text = 'Je bracht een tourniquet aan om je wond, en er ontloopt nu geen grote hoeveelheid bloed.' })
+        TriggerClientEvent("esx:showNotification", source, "Je bracht een tourniquet aan om je wond, en er ontloopt nu geen grote hoeveelheid bloed.")
         xPlayer.removeInventoryItem('tourniquet', 1)
     end)
 end
