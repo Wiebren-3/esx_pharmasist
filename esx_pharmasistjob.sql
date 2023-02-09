080f08e44e6776e19b99c793b53d8634b5dab0cf
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-09 11:43:31 +0100

    tweak (esx_pharmasistjob) aanpassingen bedragen

--- esx_pharmasistjob.sql
+++ esx_pharmasistjob.sql
@@ -17,11 +17,11 @@ INSERT INTO `jobs` (name, label) VALUES
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('pharmasist',0,'recruit','Stagair',20,'{}','{}'),
-	('pharmasist',1,'supplier','Leverancier',40,'{}','{}'),
-	('pharmasist',2,'store_employe','Winkelmedewerker',60,'{}','{}'),
-	('pharmasist',3,'lab_employe','Labmedewerker',85,'{}','{}'),
-	('pharmasist',4,'boss','Beheerder',100,'{}','{}')
+	('pharmasist',0,'recruit','Stagair',200,'{}','{}'),
+	('pharmasist',1,'supplier','Leverancier',250,'{}','{}'),
+	('pharmasist',2,'store_employe','Winkelmedewerker',250,'{}','{}'),
+	('pharmasist',3,'lab_employe','Labmedewerker',300,'{}','{}'),
+	('pharmasist',4,'boss','Beheerder',400,'{}','{}')
 ;
 
 CREATE TABLE `fine_types` (
@@ -35,7 +35,7 @@ CREATE TABLE `fine_types` (
 
 
 INSERT INTO `fine_types` (label, amount, category) VALUES
-	('Usage abusif du klaxon',30,0),
-	('Franchir une ligne continue',40,0),
-	('Circulation à contresens',250,0),
-	('Demi-tour non autorisé',250,0),
+	('Pijnstiller 1x',3000,0),
+	('Tourniquet 1x',4500,0),
+	('Verbandrol 1x',2500,0),
+	('Viteminedrinken 1x',500,0),
