aa0d90f2939bb39c09071f8c06494d1f38da9151
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-02-10 12:09:39 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,18 +1,18 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+description 'ESX Pharmacy'
 
-files {
+version '1.0.0 Alpha'
 
-'handling.meta',
-'vehicles.meta',
-'carvariations.meta',
-'carcols.meta'
-
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua'
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-
--- specfiy the names of the items being added within this (same as in the lang file in a dlc pack(kinda, the formatting is different))
-client_script 'vehicle_names.lua'
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
+	'server/main.lua'
+}
